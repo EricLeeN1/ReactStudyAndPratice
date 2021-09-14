bba74492163c0ee0fc54e148233be329c5274385
EricLeeN1 <179310009@qq.com>
2021-09-14 08:22:47 +0800

    feac:this指向问题

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logo: logo,
+      isBff: false,
+    };
+    console.log(this);
+    // 解决changeState中this指向问题
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState() {
+    this.setState((state) => ({
+      isBff: !state.isBff,
+    }));
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={this.state.logo} className="App-logo" alt="logo" />
+          <p onClick={this.changeState}>
+            Edit <code>src/App.js</code> and save to reload.{" "}
+            {this.state.isBff ? "炎热" : "寒冷"}
+          </p> 
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
